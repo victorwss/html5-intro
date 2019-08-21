b278ca44ea711ba9ef49592e68dcaca242fcdf66
Yuri Dirickson <ydirickson@gmail.com>
2019-08-20 22:37:03 -0300

    Adicionando o Gulp e Node para execução automática

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const jasmineBrowser = require('gulp-jasmine-browser');
+
+gulp.task('jasmine', function() {
+  return gulp.src(['src/**/*.html', 'spec/**/*_spec.js'], {cwd: '.'})
+    .pipe(jasmineBrowser.specRunner())
+    .pipe(jasmineBrowser.server({port: 8888}));
+});
+
+gulp.task('headless', function() {
+    return gulp.src(['src/**/*.html', 'spec/**/*_spec.js'], {cwd: 'src'})
+      .pipe(jasmineBrowser.specRunner({ console: true }))
+      .pipe(jasmineBrowser.headless({ driver: 'chrome' }));
+  });
--- helper/LoaderHelper.js
+++ helper/LoaderHelper.js
@@ -1,1 +1,22 @@
-var loadHtml=function(n,t){var e=document.getElementById("fixture-holder");return fetch(n).then(function(n){return n.text()}).then(function(n){e.innerHTML=n,t()}).catch(function(n){console.error(n)})};
+const preparar = (done) => {
+    let $div = document.createElement('div')
+    $div.id = 'fixtures'
+    document.querySelector('body').appendChild($div)
+    done()
+}
+
+const carregar = (fixture, done) => {
+    fixture = window.location.pathname === "/SpecRunner.html" ? 
+              'src/'+fixture : 
+              fixture
+    
+    fetch(fixture)
+        .then((response) => response.text())
+        .then((html) => {
+            document.querySelector('div#fixtures').innerHTML = html
+            done()
+        })
+}
+
+const limpar = () => document.querySelector('div#fixtures').innerHTML = ''
+
