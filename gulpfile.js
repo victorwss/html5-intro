c13b8a77d8afa84952c596f205ef61a6af5c7b31
Yuri Dirickson <ydirickson@gmail.com>
2019-08-27 19:01:54 -0300

    Acertando build com helper

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require('gulp');
 const jasmineBrowser = require('gulp-jasmine-browser');
 
 gulp.task('jasmine', function() {
-  return gulp.src(['src/**/*.html', 'spec/**/*_spec.js'], {cwd: '.'})
+  return gulp.src(['src/**/*.html', 'helper/**/*.js', 'spec/**/*Spec.js'], {cwd: '.'})
     .pipe(jasmineBrowser.specRunner())
     .pipe(jasmineBrowser.server({port: 8888}));
 });
 
 gulp.task('headless', function() {
-    return gulp.src(['src/**/*.html', 'spec/**/*_spec.js'], {cwd: 'src'})
+    return gulp.src(['src/**/*.html', 'spec/**/*Spec.js'], {cwd: 'src'})
       .pipe(jasmineBrowser.specRunner({ console: true }))
       .pipe(jasmineBrowser.headless({ driver: 'chrome' }));
   });
